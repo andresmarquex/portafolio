4562da58e12fb3ee8f0f2041f50bde2e284172b5
andresmarquex <andresmarquez@gmail.com>
2023-11-26 15:10:43 -0500

    v0.78
    .

--- js/index.js
+++ js/index.js
@@ -3,3 +3,18 @@ windows.addEventListener('load', () =>{
     contenedor_loader.style.opacity = 0
     contenedor_loader.style.visibility = 'hidden'
 })
+
+var video = document.getElementById("video1")
+
+window.addEventListener("scroll", function() {
+    var videoPosition = video.getBoundingClientRect();
+    var windowHeight = window.innerHeight;
+    if (videoPosition.top + videoPosition.height >= 0 && videoPosition.bottom - videoPosition.height <= windowHeight) {
+    video.play();
+    } else {
+    video.pause();
+    if (window.scrollY === 0) {
+        video.currentTime = 0;
+    }
+    }
+})
