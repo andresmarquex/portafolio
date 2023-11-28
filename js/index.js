9687ac4fecbc2a13d0ba9277430281f1de74e755
andresmarquex <andresmarquez@gmail.com>
2023-11-27 23:25:15 -0500

    v0.83
    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,48 @@
 function hideObject(id) {
     document.getElementById(id).style.display = 'none';
   }
+  
+  const images = document.querySelectorAll('.thumbnail img');
+  const selectedImage = document.querySelector('.selected-image');
+  const prev = document.querySelector('.prev');
+  const next = document.querySelector('.next');
+  let currentIndex = 0;
 
+  function showImage(index) {
+    selectedImage.src = images[index].src;
+    currentIndex = index;
+  }
+
+  function nextImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    showImage(currentIndex);
+  }
+
+  function prevImage() {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+    showImage(currentIndex);
+  }
+
+  function autoChange() {
+    nextImage();
+  }
+
+  setInterval(autoChange, 5000);
+
+  images.forEach((image, index) => {
+    image.addEventListener('click', () => {
+      showImage(index);
+    });
+  });
+
+  prev.addEventListener('click', prevImage);
+  next.addEventListener('click', nextImage);
 windows.addEventListener('load', () =>{
     const contenedor_loader=document.querySelector('.contenedor_loader')
     contenedor_loader.style.opacity = 0
@@ -22,3 +63,4 @@ window.addEventListener("scroll", function() {
     }
     }
 })
+
