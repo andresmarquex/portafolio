73799464349c648742b30286c33d9c9d4b2a8449
andresmarquex <andresmarquez@gmail.com>
2025-09-03 00:33:57 -0500

    .
    artistas v01

--- js/index.js
+++ js/index.js
@@ -1,66 +1,12 @@
-function hideObject(id) {
-    document.getElementById(id).style.display = 'none';
-  }
-  
-  const images = document.querySelectorAll('.thumbnail img');
-  const selectedImage = document.querySelector('.selected-image');
-  const prev = document.querySelector('.prev');
-  const next = document.querySelector('.next');
-  let currentIndex = 0;
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
-  function showImage(index) {
-    selectedImage.src = images[index].src;
-    currentIndex = index;
-  }
+navToggle.addEventListener('click', () => {
+	document.body.classList.toggle('nav-open');
+});
 
-  function nextImage() {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-    showImage(currentIndex);
-  }
-
-  function prevImage() {
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = images.length - 1;
-    }
-    showImage(currentIndex);
-  }
-
-  function autoChange() {
-    nextImage();
-  }
-
-  setInterval(autoChange, 5000);
-
-  images.forEach((image, index) => {
-    image.addEventListener('click', () => {
-      showImage(index);
-    });
-  });
-
-  prev.addEventListener('click', prevImage);
-  next.addEventListener('click', nextImage);
-windows.addEventListener('load', () =>{
-    const contenedor_loader=document.querySelector('.contenedor_loader')
-    contenedor_loader.style.opacity = 0
-    contenedor_loader.style.visibility = 'hidden'
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		document.body.classList.remove('nav-open');
+	})
 })
-
-var video = document.getElementById("video1")
-
-window.addEventListener("scroll", function() {
-    var videoPosition = video.getBoundingClientRect();
-    var windowHeight = window.innerHeight;
-    if (videoPosition.top + videoPosition.height >= 0 && videoPosition.bottom - videoPosition.height <= windowHeight) {
-    video.play();
-    } else {
-    video.pause();
-    if (window.scrollY === 0) {
-        video.currentTime = 0;
-    }
-    }
-})
-
